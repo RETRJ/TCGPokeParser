f684214056a5cc582d515e7f8939fe0bdc938558
RETRJ <retrj986@gmail.com>
2025-02-28 22:37:38 +0100

    added typings

--- tcg/TCGPokeInterface.py
+++ tcg/TCGPokeInterface.py
@@ -1,14 +1,14 @@
 from abc import ABC, abstractmethod
-
+from typing import List, Dict
 
 
 class TCGPokeInterface(ABC):
     @abstractmethod
-    def get_card_ids(self):
+    def get_card_ids(self) -> List[int]:
         raise NotImplementedError('Not implemented')
 
     @abstractmethod
-    def get_card_by_id(self):
+    def get_card_by_id(self, card_id: int) -> Dict[str, str | int]:
         raise NotImplementedError('Not implemented')
 
 
