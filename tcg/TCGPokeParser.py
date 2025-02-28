190d8f8ce631d289de95a5c610db211701ef1b0a
RETRJ <retrj986@gmail.com>
2025-02-28 22:51:34 +0100

    done implementing funcs

--- tcg/TCGPokeParser.py
+++ tcg/TCGPokeParser.py
@@ -1,5 +1,36 @@
+from typing import Dict, List
+
 from .TCGPokeInterface import TCGPokeInterface
 
 
 class TCGPokeParser(TCGPokeInterface):
-    pass
+    def __init__(self):
+        self.__database_example = {
+            4 : {
+                'name':'pookachu',
+                'rarity':'SAR',
+                'quantity':69
+            },
+            46: {
+                'name': 'eevee',
+                'rarity': 'U',
+                'quantity': 1286
+            },
+            27: {
+                'name': 'mega aboba',
+                'rarity': 'SSSR promo',
+                'quantity': 1,
+                'extra tag': 'golden'
+            }
+        }
+
+    def get_card_ids(self) -> List[int]:
+        return list(self.__database_example.keys())
+
+    def get_card_by_id(self, card_id: int) -> Dict[str, str | int]:
+        return self.__database_example[card_id]
+
+
+
+
+
