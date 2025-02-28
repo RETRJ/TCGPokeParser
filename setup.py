a8a48910d907874855607d423f6b216b63b24879
RETRJ <retrj986@gmail.com>
2025-02-28 22:12:24 +0100

    fixes

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
 
+
 setup(
     name='TCGPokeParser',
     version='0.1',
     packages=find_packages(),
     description='TCGPokeParser',
     long_description=open('README.md').read(),
-    install_requires=[i for i in open('requirements.txt').readlines()],
+    install_requires=[]
 )
